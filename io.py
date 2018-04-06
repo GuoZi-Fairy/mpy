039d5f3b3d1ccae45b09e1d6cce0328265c59f57
bambomMan <2369941198@qq.com>
2018-04-07 00:50:37 +0800

    bambomMan

--- io.py
+++ io.py
@@ -1,5 +1,21 @@
 import os
 import sys
 
+
 def __std_input__():
-    
+    "Return None normally"
+    length = len(sys.argv)
+    if length > 1:
+        for i in range(1,length):
+            if os.path.exists(i) and os.path.isfile(i):
+                file_name = os.path.split(i)[1] # (foo1,foo2)<-Tuple[1]
+                with open(i) as f:
+                    for line in f:
+                        print(line)
+                        return None
+            else:
+                return 'wrong path'
+    else:
+        return 'Missing Parameters'
+
+__std_input__()
